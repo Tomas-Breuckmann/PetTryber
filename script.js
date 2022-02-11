1e752f7c84cf1598c07edea9de84850939595f52
LDantasA <leonardo.dantas0@gmail.com>
2022-02-11 18:39:48 -0300

    Listagem dos animais

--- script.js
+++ script.js
@@ -8,31 +8,58 @@ window.onload = () => {
 function configuraBotoes() {
   const botaoCat = document.querySelector('.cat');
   const botaoDog = document.querySelector('.dog');
-  botaoCat.addEventListener('click', () => listAnimals('Cat'))
-  botaoDog.addEventListener('click', () => listAnimals('Dog'))
+  botaoCat.addEventListener('click', () => getAnimals('Cat'));
+  botaoDog.addEventListener('click', () => getAnimals('Dog'));
 }
 
-async function listAnimals(specie) {
+function createCustomElement(tag, theClass, content) {
+  const element = document.createElement(tag);
+  element.className = theClass;
+  if (content) element.innerHTML = content;
+  return element;
+}
+
+function photoLocalizer(photo) {
+  return photo ? photo.small : 'https://c.tenor.com/RVvnVPK-6dcAAAAM/reload-cat.gif';
+}
+
+function generateAnimalElements(animal) {
+  const animalsList = document.querySelector('.available-animals');
+  const { name, age, gender, breeds, pagination, primary_photo_cropped } = animal;
+  const animalElement = createCustomElement('li', 'animal-container');
+  const img = createCustomElement('img', 'animal-image');
+  img.src = photoLocalizer(primary_photo_cropped);
+
+  animalElement.appendChild(img)
+  animalElement.appendChild(createCustomElement('p', 'animal-name', `<strong>Nome:</strong> ${name}`));
+  animalsList.appendChild(animalElement);
+}
+
+function listAnimals(list) {
+  const { animals, pagination } = list;
+  animals.forEach(generateAnimalElements);
+}
+
+async function getAnimals(specie) {
   const result = await fetchAnimals(specie);
-  const lista = Array.from(result.animals)
-  // console.log(lista);
+  listAnimals(result)
 }
 
 // Random Animals 
 
 
 const animalsRandom = () => {
-  
-  const dog = document.querySelector('#dog')
-  const cat = document.querySelector('#cat')
-  const others = document.querySelector('#others')
+  const random = (maxNumber) => Math.floor(Math.random() * maxNumber) + 1;
 
-  const random = (maxNumber) => Math.floor(Math.random() * maxNumber) + 1
-  const dogRandom = random(10)
-  const catRandom = random(10)
-  const othersRandom = random(10)
+  const dog = document.querySelector('#dog');
+  const cat = document.querySelector('#cat');
+  const others = document.querySelector('#others');
 
-  dog.setAttribute('src', `images/pet-dog-${dogRandom}.png`)
-  cat.setAttribute('src', `images/pet-cat-${catRandom}.png`)
-  others.setAttribute('src', `images/pet-other-${othersRandom}.png`)
+  const dogRandom = random(10);
+  const catRandom = random(10);
+  const othersRandom = random(10);
+
+  dog.setAttribute('src', `images/pet-dog-${dogRandom}.png`);
+  cat.setAttribute('src', `images/pet-cat-${catRandom}.png`);
+  others.setAttribute('src', `images/pet-other-${othersRandom}.png`);
 }

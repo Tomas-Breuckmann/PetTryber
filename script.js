a0c9b1d3f4ebf557f235b3fa182a6b4c084bdfa8
Ramond Falcão <ramondfalcao@gmail.com>
2022-02-11 16:52:46 -0300

    função animalsRandom

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 window.onload = () => {
   // let result = await fetchAnimals();
   // console.log(result);
-  configuraBotoes()
+  animalsRandom();
+  configuraBotoes();
 }
 
 function configuraBotoes() {
@@ -15,4 +16,23 @@ async function listAnimals(specie) {
   const result = await fetchAnimals(specie);
   const lista = Array.from(result.animals)
   // console.log(lista);
+}
+
+// Random Animals 
+
+
+const animalsRandom = () => {
+  
+  const dog = document.querySelector('#dog')
+  const cat = document.querySelector('#cat')
+  const others = document.querySelector('#others')
+
+  const random = (maxNumber) => Math.floor(Math.random() * maxNumber) + 1
+  const dogRandom = random(10)
+  const catRandom = random(10)
+  const othersRandom = random(10)
+
+  dog.setAttribute('src', `images/pet-dog-${dogRandom}.png`)
+  cat.setAttribute('src', `images/pet-cat-${catRandom}.png`)
+  others.setAttribute('src', `images/pet-other-${othersRandom}.png`)
 }

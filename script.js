94ac29d84f63faa1ef2f5ae47358278841600a55
phvleite <phvleite@gmail.com>
2022-02-13 16:24:34 -0300

    retirado a opção OTHERS

--- script.js
+++ script.js
@@ -34,17 +34,17 @@ function closePopUp(event) {
 function configuraBotoes() {
   const botaoCat = document.querySelector('#options-cat');
   const botaoDog = document.querySelector('#options-dog');
-  const botaoOthers = document.querySelector('#options-other');
+  // const botaoOthers = document.querySelector('#options-other');
   botaoCat.addEventListener('click', () => getAnimals('Cat'));
   botaoDog.addEventListener('click', () => getAnimals('Dog'));
-  botaoOthers.addEventListener('click', () => {
-      const fEspecie = Array.from(document.getElementsByName('especie'));
-      // console.log(fEspecie);
-      const sEspecie = fEspecie.find((element) => element.checked === true)
-      console.log(sEspecie.value);
-      console.log(typeof(sEspecie.value));
-      getAnimals(sEspecie.value);
-  });
+  // botaoOthers.addEventListener('click', () => {
+  //     const fEspecie = Array.from(document.getElementsByName('especie'));
+  //     // console.log(fEspecie);
+  //     const sEspecie = fEspecie.find((element) => element.checked === true)
+  //     console.log(sEspecie.value);
+  //     console.log(typeof(sEspecie.value));
+  //     getAnimals(sEspecie.value);
+  // });
   
   const closePopupButton = document.querySelector('.close-popup')
   closePopupButton.addEventListener('click', closePopUp)
@@ -169,8 +169,8 @@ async function getPet({ target }) {
 }
 
 async function getAnimals(specie) {
-  const selectedSpecie = (specie !== 'Dog' && specie !== 'Cat') ? document.querySelector('#options-other'): document.querySelector(`#options-${specie.toLowerCase()}`);
-  // const selectedSpecie = document.querySelector(`#options-${specie.toLowerCase()}`);
+  // const selectedSpecie = (specie !== 'Dog' && specie !== 'Cat') ? document.querySelector('#options-other'): document.querySelector(`#options-${specie.toLowerCase()}`);
+  const selectedSpecie = document.querySelector(`#options-${specie.toLowerCase()}`);
   clearSelectedItem('.item-selected')
   selectedSpecie.classList.add('item-selected');
   if (currentAnimal !== specie) {
@@ -189,13 +189,13 @@ const animalsRandom = () => {
 
   const dog = document.querySelector('#dog');
   const cat = document.querySelector('#cat');
-  const others = document.querySelector('#others');
+  // const others = document.querySelector('#others');
 
   const dogRandom = random(10);
   const catRandom = random(10);
-  const othersRandom = random(10);
+  // const othersRandom = random(10);
 
   dog.setAttribute('src', `images/pet-dog-${dogRandom}.png`);
   cat.setAttribute('src', `images/pet-cat-${catRandom}.png`);
-  others.setAttribute('src', `images/pet-other-${othersRandom}.png`);
+  // others.setAttribute('src', `images/pet-other-${othersRandom}.png`);
 }
